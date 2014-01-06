fceba8ab50492bd15be61cfcf4767ba078ddf79d
Trond Kjeldås <trond@kjeldas.no>
2014-01-06 21:04:54 +0100

    Added licensing notification in file header.

--- KnxDimmerControl.h
+++ KnxDimmerControl.h
@@ -1,11 +1,23 @@
 //
 //  KnxDimmerControl.h
-//  KnxSubscriber
 //
-//  Created by Trond Kjeldås on 05.08.11.
-//  Copyright 2011 Systemsoft AS. All rights reserved.
+//  Created by Trond Kjeldås on 05.08.11. Copyright 2011, All rights reserved.
 //
-
+// This file is part of the KnxBasics library.
+//
+// The KnxBasics is free software: you can redistribute it and/or modify
+// it under the terms of the Lesser GNU General Public License version 2.1,
+// as published by the Free Software Foundation.
+//
+// The KnxBasics library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// Lesser GNU General Public License for more details.
+//
+// You should have received a copy of the Lesser GNU General Public License
+// along with the KnxBasics library. If not, see <http://www.gnu.org/licenses/>.
+//
+//
 #import <Foundation/Foundation.h>
 
 #import "KnxGroupAddress.h"
--- KnxTelegram.m
+++ KnxTelegram.m
@@ -1,11 +1,23 @@
 //
 //  KnxTelegram.m
 //  
+//  Created by Trond Kjeldås on 04.09.11. Copyright 2011, All rights reserved.
 //
-//  Created by Trond Kjeldås on 04.09.11.
-//  Copyright 2011 Systemsoft AS. All rights reserved.
+// This file is part of the KnxBasics library.
 //
-
+// The KnxBasics is free software: you can redistribute it and/or modify
+// it under the terms of the Lesser GNU General Public License version 2.1,
+// as published by the Free Software Foundation.
+//
+// The KnxBasics library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// Lesser GNU General Public License for more details.
+//
+// You should have received a copy of the Lesser GNU General Public License
+// along with the KnxBasics library. If not, see <http://www.gnu.org/licenses/>.
+//
+//
 #import "KnxTelegram.h"
 
 
